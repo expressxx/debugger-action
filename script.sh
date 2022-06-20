8db1cd012442f9fde361eafd020f930a85aa6b2b
expressxx <80801005+expressxx@users.noreply.github.com>
2022-06-20 13:11:44 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,7 +29,15 @@ echo ___________________________________________________________________________
 echo
 echo To connect to this session copy-n-paste the following into a terminal:
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
-echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout
+echo After connecting you can run 'touch /tmp/keepalive' to disable the 100m timeout
+
+touch /tmp/keepalive
+
+git config --global user.email "github-actions@github.com"
+git config --global user.name "github-actions"
+
+cd ~/work
+git clone https://github.com/gl-inet/gl-infra-builder.git
 
 if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
   MSG=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
@@ -37,7 +45,7 @@ if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
 fi
 
 # Wait for connection to close or timeout in 15 min
-timeout=$((100*60))
+timeout=$((120*60))
 while [ -S /tmp/tmate.sock ]; do
   sleep 1
   timeout=$(($timeout-1))
